2a7a291911f55029ca9cce80ae2090c079c6f6cd
Marc-Andre <marcandr.girard@gmail.com>
2016-06-09 19:34:25 -0400

    Fix creation of the grid

--- Grid.rb
+++ Grid.rb
@@ -2,21 +2,25 @@ class Grid
   require 'Case'
 
   def initialize
-    @cases = [(1..10).each { |n| Case.new(n)},
-      (11..20).each { |n| Case.new(n)},
-      (21..30).each { |n| Case.new(n)},
-      (31..40).each { |n| Case.new(n)},
-      (41..50).each { |n| Case.new(n)},
-      (51..60).each { |n| Case.new(n)},
-      (61..70).each { |n| Case.new(n)},
-      (71..80).each { |n| Case.new(n)},
-      (81..90).each { |n| Case.new(n)},
-      (91..100).each { |n| Case.new(n)}]
+    @cases = []
+    10.times do |i|
+      lower_bound = (i * 10) + 1
+      @cases.push((lower_bound..lower_bound + 9).each { |n| Case.new(n)})
+    end
+
   end
 
   def print_to_console
+    reverse = false
     @cases.reverse_each  do  |row|
-      row.each { |element| print "| #{element.to_s} " }
+      if reverse
+        row.reverse_each { |element| print "| #{element.to_s} " }
+        reverse = false
+      else
+        row.each { |element| print "| #{element.to_s} " }
+        reverse = true
+      end
+      
       print "|"
       puts ""
     end
